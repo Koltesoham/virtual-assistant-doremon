9805480953df5b814a6cf487187b6f141c625876
Koltesoham <koltesoham2008@gmail.com>
2025-10-26 17:42:06 +0530

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        lblResponse = New Label()
+        btnExecute = New Button()
+        txtCommand = New TextBox()
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' lblResponse
+        ' 
+        lblResponse.AutoSize = True
+        lblResponse.BackColor = SystemColors.ActiveCaptionText
+        lblResponse.ForeColor = SystemColors.ButtonFace
+        lblResponse.Location = New Point(174, 206)
+        lblResponse.Name = "lblResponse"
+        lblResponse.Size = New Size(0, 15)
+        lblResponse.TabIndex = 0
+        ' 
+        ' btnExecute
+        ' 
+        btnExecute.BackColor = Color.Lime
+        btnExecute.Location = New Point(561, 344)
+        btnExecute.Name = "btnExecute"
+        btnExecute.Size = New Size(75, 23)
+        btnExecute.TabIndex = 1
+        btnExecute.Text = "send"
+        btnExecute.UseVisualStyleBackColor = False
+        ' 
+        ' txtCommand
+        ' 
+        txtCommand.BackColor = Color.Silver
+        txtCommand.Location = New Point(12, 344)
+        txtCommand.Multiline = True
+        txtCommand.Name = "txtCommand"
+        txtCommand.Size = New Size(543, 23)
+        txtCommand.TabIndex = 2
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Snap ITC", 36F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.Blue
+        Label1.Location = New Point(174, 39)
+        Label1.MaximumSize = New Size(1000, 1000)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(349, 63)
+        Label1.TabIndex = 3
+        Label1.Text = "DOREMON "
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaptionText
+        ClientSize = New Size(663, 409)
+        Controls.Add(Label1)
+        Controls.Add(txtCommand)
+        Controls.Add(btnExecute)
+        Controls.Add(lblResponse)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Form1"
+        Text = "DOREMON"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblResponse As Label
+    Friend WithEvents btnExecute As Button
+    Friend WithEvents txtCommand As TextBox
+    Friend WithEvents Label1 As Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,65 @@
+﻿Imports System.Speech.Synthesis
+
+
+Public Class Form1
+    Dim synth As New SpeechSynthesizer()
+
+    Private Sub btnExecute_Click(sender As Object, e As EventArgs) Handles btnExecute.Click
+        Dim command As String = txtCommand.Text.ToLower().Trim()
+
+        Select Case command
+            Case "hello"
+                lblResponse.Text = "Hello! How can I help you?"
+                synth.SpeakAsync("Hello! How can I help you?")
+            Case "hi"
+                lblResponse.Text = "Hello! How can I help you?"
+                synth.SpeakAsync("Hello! How can I help you?")
+
+            Case "open youtube"
+                lblResponse.Text = "Opening YouTube..."
+                synth.SpeakAsync("Opening YouTube")
+                Process.Start(New ProcessStartInfo("https://www.youtube.com") With {.UseShellExecute = True})
+
+
+            Case "open calculator"
+                lblResponse.Text = "Opening Calculator..."
+                synth.SpeakAsync("Opening Calculator")
+                Process.Start("calc.exe")
+
+            Case "good bye"
+                lblResponse.Text = "Goodbye!"
+                synth.SpeakAsync("Goodbye!")
+                Me.Close()
+
+            Case "what is your name"
+                lblResponse.Text = "I am your doremon virtual assistant."
+                synth.SpeakAsync("I am your doremon virtual assistant.")
+            Case "what time is it"
+                Dim currentTime As String = DateTime.Now.ToString("h:mm tt")
+                lblResponse.Text = "The current time is " & currentTime
+                synth.SpeakAsync("The current time is " & currentTime)
+            Case "open notepad"
+                lblResponse.Text = "Opening Notepad..."
+                synth.SpeakAsync("Opening Notepad")
+                Process.Start("notepad.exe")
+            Case "tell me a joke"
+                lblResponse.Text = "Aek Tha Raja Aek Thi Rani Or Fir Khatan Hogayi Khani!!"
+                synth.SpeakAsync("Aek Tha Raja Aek Thi Rani Or Fir Khatan Hogayi Khani!!")
+            Case "who is your developer"
+                lblResponse.Text = "I was developed by Soham Kolte."
+                synth.SpeakAsync("I was developed by Soham Kolte.")
+            Case "open google"
+                lblResponse.Text = "Opening Google..."
+                synth.SpeakAsync("Opening Google")
+                Process.Start(New ProcessStartInfo("https://www.google.com") With {.UseShellExecute = True})
+
+            Case Else
+                lblResponse.Text = "Sorry, I didn't understand that."
+                synth.SpeakAsync("Sorry, I didn't understand that.")
+        End Select
+    End Sub
+
+
+
+End Class
+
